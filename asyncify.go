5612175dbd7f724a3629218c32ce9230408c2110
iamando <iamando@tuta.io>
2023-05-05 18:33:41 +0300

    feature: re-arch module use constant and types

--- asyncify.go
+++ asyncify.go
@@ -1,114 +1,156 @@
 package asyncify
 
-// Resolve returns a new Promise that is already resolved with the given value.
-// The returned Promise's `result` channel will receive the value immediately
-// after the function call. The `err` channel will not be used.
-func Resolve(val interface{}) *Promise {
-	promise := &Promise{
-		result: make(chan interface{}),
-		err:    make(chan error),
+// Promise creates and returns a new Promise object that represents the eventual completion
+// (or failure) of an asynchronous operation, and executes the specified executor function
+// immediately. The executor function takes two arguments, a resolve function and a reject function,
+// that allow the promise to be resolved with a value or rejected with a reason, respectively.
+// The Promise object returned by this function has methods `Then`, `Catch`, and `Finally`
+// that allow for chaining and handling of the eventual fulfillment or rejection of the promise.
+// The Promise object also provides a blocking `Await` method that can be used to wait for the
+// promise to be resolved or rejected.
+func Promise(executor func(resolve func(interface{}), reject func(error))) *promise {
+	p := &promise{
+		state:     pending,
+		awaitChan: make(chan struct{}),
 	}
 
-	go func() {
-		promise.result <- val
-	}()
-
-	return promise
-}
-
-// Reject returns a new Promise that is already rejected with the given error.
-// The returned Promise's `err` channel will receive the error immediately
-// after the function call. The `result` channel will not be used.
-func Reject(err error) *Promise {
-	promise := &Promise{
-		result: make(chan interface{}),
-		err:    make(chan error),
-	}
-
-	go func() {
-		promise.err <- err
-	}()
-
-	return promise
-}
-
-// Then appends a new callback function to the promise's `callbacks` list.
-// The callback function is invoked when the promise is resolved, and the
-// value returned by the callback is used to resolve the new promise that
-// is returned by this function. If the callback throws an error, the new
-// promise is rejected with the error. The callback function can return a
-// value or a Promise object, and the new promise returned by this method
-// will be resolved or rejected based on the result of the callback.
-func (p *Promise) Then(fn func(interface{}) interface{}) *Promise {
-	promise := &Promise{
-		result: make(chan interface{}),
-		err:    make(chan error),
-	}
-
-	go func() {
-		select {
-		case val := <-p.result:
-			res := fn(val)
-
-			if p, ok := res.(*Promise); ok {
-				promise.result = p.result
-				promise.err = p.err
-			} else {
-				promise.result <- res
-			}
-
-		case err := <-p.err:
-			promise.err <- err
+	resolve := func(result interface{}) {
+		if p.state != pending {
+			return
 		}
-	}()
 
-	return promise
-}
-
-// Catch appends a new error callback function to the promise's `callbacks` list.
-// The callback function is invoked when the promise is rejected, and the value
-// returned by the callback is used to resolve the new promise that is returned
-// by this function. If the callback throws an error, the new promise is rejected
-// with the error. The callback function can return a value or a Promise object,
-// and the new promise returned by this method will be resolved or rejected based
-// on the result of the callback.
-func (p *Promise) Catch(fn func(error) interface{}) *Promise {
-	promise := &Promise{
-		result: make(chan interface{}),
-		err:    make(chan error),
-	}
-
-	go func() {
-		select {
-		case val := <-p.result:
-			promise.result <- val
-
-		case err := <-p.err:
-			res := fn(err)
-
-			if p, ok := res.(*Promise); ok {
-				promise.result = p.result
-				promise.err = p.err
-			} else {
-				promise.err <- err
-			}
+		if p.thenFn != nil {
+			p.result = p.thenFn(result)
+		} else {
+			p.result = result
 		}
-	}()
 
-	return promise
+		p.state = fulfilled
+
+		if p.finallyFn != nil {
+			p.finallyFn()
+		}
+
+		close(p.awaitChan)
+	}
+
+	reject := func(err error) {
+		if p.state != pending {
+			return
+		}
+
+		if p.catchFn != nil {
+			p.result = p.catchFn(err)
+		} else {
+			p.err = err
+		}
+
+		p.state = rejected
+
+		if p.finallyFn != nil {
+			p.finallyFn()
+		}
+
+		close(p.awaitChan)
+	}
+
+	go executor(resolve, reject)
+
+	return p
 }
 
-// Await blocks until the promise is resolved or rejected,
-// then returns the result or error respectively.
-// If the promise is resolved, the first return value is the result
-// and the second is nil.
-// If the promise is rejected, the first return value is nil
-// and the second is the error.
-func (p *Promise) Await() (interface{}, error) {
-	select {
-	case val := <-p.result:
-		return val, nil
-	case err := <-p.err:
-		return nil, err
+// Then registers a callback function to be called when the promise is resolved. If the promise is already resolved, the callback is called immediately with the resolved value. If the promise is rejected, the callback is skipped.
+// The callback function takes one argument, the resolved value of the promise, and should return a value or a new promise that will be resolved with that value.
+// Returns a new promise that is resolved with the return value of the callback function or rejected with the same reason as the original promise, if the callback function throws an error.
+func (p *promise) Then(fn func(interface{}) interface{}) *promise {
+	Promise := &promise{
+		state:     pending,
+		awaitChan: make(chan struct{}),
 	}
+
+	p.thenFn = fn
+
+	if p.state == fulfilled {
+		if p.thenFn != nil {
+			Promise.result = p.thenFn(p.result)
+		} else {
+			Promise.result = p.result
+		}
+
+		Promise.state = fulfilled
+
+		if p.finallyFn != nil {
+			p.finallyFn()
+		}
+
+		close(Promise.awaitChan)
+	} else if p.state == rejected {
+		if p.catchFn != nil {
+			Promise.result = p.catchFn(p.err)
+		} else {
+			Promise.err = p.err
+		}
+
+		Promise.state = rejected
+
+		if p.finallyFn != nil {
+			p.finallyFn()
+		}
+
+		close(Promise.awaitChan)
+	}
+
+	return Promise
+}
+
+// Catch registers a callback function to be called when the promise is rejected. If the promise is already rejected, the callback is called immediately with the rejection reason. If the promise is resolved, the callback is skipped.
+// The callback function takes one argument, the rejection reason of the promise, and should return a value or a new promise that will be resolved with that value.
+// Returns a new promise that is resolved with the return value of the callback function or rejected with the same reason as the original promise, if the callback function throws an error.
+func (p *promise) Catch(fn func(error) interface{}) *promise {
+	Promise := &promise{
+		state:     pending,
+		awaitChan: make(chan struct{}),
+	}
+
+	p.catchFn = fn
+
+	if p.state == fulfilled {
+		Promise.result = p.result
+		Promise.state = fulfilled
+
+		if p.finallyFn != nil {
+			p.finallyFn()
+		}
+
+		close(Promise.awaitChan)
+	} else if p.state == rejected {
+		if p.catchFn != nil {
+			Promise.result = p.catchFn(p.err)
+		} else {
+			Promise.err = p.err
+		}
+
+		Promise.state = rejected
+
+		if p.finallyFn != nil {
+			p.finallyFn()
+		}
+
+		close(Promise.awaitChan)
+	}
+
+	return Promise
+}
+
+// Finally registers a callback function to be called when the promise is either resolved or rejected. If the promise is already resolved or rejected, the callback is called immediately.
+// The callback function takes no arguments and should not return anything.
+// Returns the same promise instance to allow for chaining of methods.
+func (p *promise) Finally(fn func()) *promise {
+	p.finallyFn = fn
+
+	if p.state != pending {
+		p.finallyFn()
+	}
+
+	return p
 }
--- types.go
+++ types.go
@@ -1,6 +1,13 @@
 package asyncify
 
-type Promise struct {
-	result chan interface{}
-	err    chan error
+type promiseState int
+
+type promise struct {
+	state     promiseState
+	result    interface{}
+	err       error
+	thenFn    func(interface{}) interface{}
+	catchFn   func(error) interface{}
+	finallyFn func()
+	awaitChan chan struct{}
 }
